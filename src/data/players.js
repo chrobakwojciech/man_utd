a093c43bfbbc53796cb5ffdae1875c7f79eab6c8
woitech <wojtek@uigstudio.com>
2022-09-08 17:05:18 +0200

    new playesr (dubravka, casemiro, antony)

--- src/data/players.js
+++ src/data/players.js
@@ -10,17 +10,16 @@ const goalkeepers = [
     { positionType: position.GOALKEEPER, name: 'De Gea', number: 1 },
     // { positionType: position.GOALKEEPER, name: 'Henderson', number: 26 },
     { positionType: position.GOALKEEPER, name: 'Heaton', number: 22 },
+    { positionType: position.GOALKEEPER, name: 'Dubravka', number: 31 },
 ]
 
 const defenders = [
     { positionType: position.DEFENDER, name: 'Lindelof', number: 2 },
-    { positionType: position.DEFENDER, name: 'Bailly', number: 3 },
     { positionType: position.DEFENDER, name: 'Jones', number: 4 },
     { positionType: position.DEFENDER, name: 'Maguire', number: 5 },
     { positionType: position.DEFENDER, name: 'Martinez', number: 6 },
     { positionType: position.DEFENDER, name: 'Malacia', number: 12 },
     { positionType: position.DEFENDER, name: 'Shaw', number: 23 },
-    // { positionType: position.DEFENDER, name: 'Telles', number: 27 },
     { positionType: position.DEFENDER, name: 'Wan-Bissaka', number: 29 },
     { positionType: position.DEFENDER, name: 'Dalot', number: 20 },
     { positionType: position.DEFENDER, name: 'Williams', number: 33 },
@@ -37,7 +36,7 @@ const midfielders = [
     { positionType: position.MIDFIELDER, name: 'van de Beek', number: 34 },
     { positionType: position.MIDFIELDER, name: 'McTominay', number: 39 },
     { positionType: position.MIDFIELDER, name: 'Hannibal', number: 46 },
-    { positionType: position.MIDFIELDER, name: 'Garner', number: 37 },
+    { positionType: position.MIDFIELDER, name: 'Casemiro', number: 18 },
     { positionType: position.MIDFIELDER, name: 'Amad', number: 16 },
     { positionType: position.MIDFIELDER, name: 'Savage', number: 51 },
     { positionType: position.MIDFIELDER, name: 'Iqbal', number: 55 }
@@ -48,12 +47,12 @@ const forwards = [
     { positionType: position.FORWARD, name: 'Rashford', number: 10 },
     { positionType: position.FORWARD, name: 'Greenwood', number: 11 },
     { positionType: position.FORWARD, name: 'Amad', number: 16 },
+    { positionType: position.FORWARD, name: 'Antony', number: 21 },
     { positionType: position.FORWARD, name: 'Elanga', number: 36 },
     { positionType: position.FORWARD, name: 'Shoretire', number: 47 },
     { positionType: position.FORWARD, name: 'Sancho', number: 25 },
     { positionType: position.FORWARD, name: 'Ronaldo', number: 7 },
     { positionType: position.FORWARD, name: 'Garnacho', number: 49 },
-    { positionType: position.FORWARD, name: 'Chong', number: 44 }
 ]
 
 export const defaultPlayer = { positionType: position.UNKNOWN, name: '', number: 0 };
